22ff57a4ea312c744422b2f867458785c56a4b9a
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-06-06 13:07:16 +0530

    Image View Project added to git. This Project shows how to use Image View in android using java.

---
+++ app/src/main/java/com/example/differentandroidcodes/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.differentandroidcodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
